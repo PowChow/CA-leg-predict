8639957e509221e7b59ba5bc1dc2d79cca722645
Sam Brotherton <sbrother@gmail.com>
2014-04-30 17:35:28 -0700

    some tweaks

--- LegText.py
+++ LegText.py
@@ -17,63 +17,68 @@ from optparse import OptionParser
 import sys
 from time import time
 
-logging.basicConfig(level=logging.INFO, format '%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format = '%(asctime)s %(levelname)s %(message)s')
 
 #============================================================================
 # FUNCTIONS
 #============================================================================
 
+client = MongoClient('mongodb://galaHELP:pythonisfun!@oceanic.mongohq.com:10036/openstates')
+db = client.openstates
+
 def GetLegText(link):
-	'''
-	Function fetches legislative text from url
-	@para link: string type http://http://leginfo.legislature.ca.gov/faces/billNavClient.xhtml?bill_id='session'+'bill_id'
-	'''
-	
-	html = requests.get(link).text
-	html_encode = html.encode('ascii','ignore') #convert text to string
+    '''
+    Function fetches legislative text from url
+    @para link: string type http://http://leginfo.legislature.ca.gov/faces/billNavClient.xhtml?bill_id='session'+'bill_id'
+    '''
+    
+    html = requests.get(link).text
+    html_encode = html.encode('ascii','ignore') #convert text to string
 
-	doc = fromstring(html_encode)
+    doc = fromstring(html_encode)
 
-	tags = ['h1','h2','h3','h4','h5','h6',
-	       'div', 'span', 
-	       'img', 'area', 'map']
-	args = {'meta':False, 'safe_attrs_only':False, 'page_structure':True, 
-	       'scripts':True, 'style':True, 'links':True, 'remove_tags':tags}
-	cleaner = Cleaner(**args)
+    tags = ['h1','h2','h3','h4','h5','h6',
+           'div', 'span', 
+           'img', 'area', 'map']
+    args = {'meta':False, 'safe_attrs_only':False, 'page_structure':True, 
+           'scripts':True, 'style':True, 'links':True, 'remove_tags':tags}
+    cleaner = Cleaner(**args)
 
-	path = '/html/body'
-	body = doc.xpath(path)[0]
+    path = '/html/body'
+    body = doc.xpath(path)[0]
 
-	clean_doc = (cleaner.clean_html(body).text_content().encode('ascii', 'ignore')).strip().split('Version:',1)[1]
+    clean_doc = (cleaner.clean_html(body).text_content().encode('ascii', 'ignore')).strip().split('Version:',1)[1]
 
-	return clean_doc
+    return clean_doc
 
 #============================================================================
 #Next Step - Get URL from bills details and texts
 #============================================================================
 
 def main():
-    
-    logging.basicsConfig(filename='CA_LegPredict.log', level=logging.DEBUG)
     logging.info('Started')
 
-	#list of dictionaries
-	url_query = db.bills_details.find({},
+    #list of dictionaries
+    url_query = db.bills_details.find({},
             {'_id': 0, 
              'session': 1, 'bill_id':1, 'title':1, 'subjects':1, 'versions.url':1,    
-             })
-	lod_leg = list(url_query)
+             }).limit(20)
+    lod_leg = list(url_query)
+    #add text  
 
-	#add text  
-        list_leg_url = {'text': str(GetLegText(list_leg[0][''])) for k, v in list_leg[0].iteritems}
-
+    #lod_leg_url = {'text': str(GetLegText(lod_leg[0][''])) for k, v in lod_leg[0].iteritems()}
+    one_instance = lod_leg[0]
+    print "one instance:", one_instance
+    lod_leg_url = one_instance['versions'][0]['url']
+    text GetLegText(lod_leg_url)
     #Adds string type URL and legislative text 
     #embedded url in 'versions' - str(lod_leg[0]['versions'][0].values()[0])
     for l in lod_leg[:2]: #limited to [:2] for testing and not sure of call on html limit
-    	for x in l['versions']:
-    		link = str(x.values()[0])
-    		l['url'] = link
-    		l['text'] = GetLegText(link)
+        l['urls'] = [x['url'] for x in l['versions']]
+        l['texts'] = [x['text'] for x in l['versions']]
+            link = str(x.values()[0])
+            l['url'] = link
+            l['text'] = GetLegText(link)
 
     # -----------------using one legtext as an example---------------------------------------
     db.legtext.insert(lod_leg[:2]) #limited to [:2] for testing
@@ -88,7 +93,7 @@ def main():
     [wnl.lemmatize(t) for t in words]
 
     #run through gensim to determine categories for each legislative text - lesson 15
-
+    print "Done!"
     logging.info('Finished')
 
 #============================================================================
