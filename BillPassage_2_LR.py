38deb8b5498c0a9ac9f38c7746fc575ea2b8bc00
PowChow <pauline.chow@gmail.com>
2016-10-08 19:18:31 -0700

    updated

--- BillPassage_2_LR.py
+++ BillPassage_2_LR.py
@@ -16,6 +16,7 @@ from gensim import corpora, models, similarities
 def main():
     df = pd.read_csv('./saved_models/merged_df_bills_topic.csv')
     df2 = df.iloc[:, 2:-2] 
+    print df2.head()
     x = df2.values
     xScaled = StandardScaler().fit_transform(x)
 
